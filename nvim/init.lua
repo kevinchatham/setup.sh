a8ea0964082715016b674d457a59b8bdd8f16322
kevinchatham <40923272+kevinchatham@users.noreply.github.com>
2022-10-22 23:43:20 -0500

    leader shortcuts

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,15 +17,16 @@ require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
-  -- themes use 'mjlbach/onedark.nvim'         -- onedark
+  -- themes
+  use 'mjlbach/onedark.nvim'         -- onedark
   use 'AhmedAbdulrahman/vim-aylin'   -- aylin
   use 'fratajczak/one-monokai-vim'   -- one-monokai
   use { "ellisonleao/gruvbox.nvim" } -- gruvbox
+  use 'folke/tokyonight.nvim'        -- tokyo-night-moon
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
 
-  use 'folke/tokyonight.nvim'
 
   if is_bootstrap then
     require('packer').sync()
@@ -209,7 +210,7 @@ require('gitsigns').setup {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
-    file_ignore_patterns = { "node_modules/.*" },
+    file_ignore_patterns = { "node_modules/.*", "dist/.*" },
     mappings = {
       i = {
         ['<C-u>'] = false,
@@ -221,16 +222,16 @@ require('telescope').setup {
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
+vim.keymap.set('n', '<leader>', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
     previewer = false,
   })
-end, { desc = '[/] Fuzzily search in current buffer]' })
+end, { desc = 'Search current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').find_files, { desc = 'Search Files' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
