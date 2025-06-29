ceb92122e666db1a130ebbeed64210a9d5b72ba5
kevinchatham <40923272+kevinchatham@users.noreply.github.com>
2025-06-29 15:23:18 -0500

    tf platform fix

--- setup.sh
+++ setup.sh
@@ -26,20 +26,11 @@ echo ""
 sudo apt install git -y
 git config --global user.name kevinchatham
 git config --global user.email 40923272+kevinchatham@users.noreply.github.com
-git config --global credential.helper store
-git config --global init.defaultBranch main
-git config --global pull.rebase true
 
 echo ""
 echo "----- installing curl -----"
 echo ""
 sudo apt install curl -y
-
-echo ""
-echo "----- installing ssh -----"
-echo ""
-sudo apt install openssh-server -y
-sudo service ssh start
 
 echo ""
 echo "----- installing zsh -----"
@@ -69,34 +60,6 @@ echo ""
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
 echo ""
-echo "----- installing nvim -----"
-echo ""
-sudo apt install build-essential -y
-mkdir -p ~/.nvim
-cd ~/.nvim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-cd $OWD
-
-echo ""
-echo "----- installing unzip -----"
-echo ""
-sudo apt install unzip -y
-
-# TODO For Hyper-V Console
-# echo ""
-# echo "----- installing fira code nerd font -----"
-# echo ""
-# sudo apt install unzip -y
-# rm ~/FiraCode.zip
-# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip ~
-# mkdir -p ~/.fonts
-# unzip ~/FiraCode.zip -d ~/.fonts
-# fc-cache -fv # idk about this
-# rm ~/FiraCode.zip
-
-echo ""
 echo "----- installing neofetch -----"
 echo ""
 sudo apt install neofetch -y
@@ -107,9 +70,9 @@ echo ""
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 echo ""
-echo "----- installing dotnet 6.0 -----"
+echo "----- installing dotnet 9.0 -----"
 echo ""
-sudo apt install -y dotnet-sdk-6.0
+sudo apt install -y dotnet-sdk-9.0
 
 echo ""
 echo "----- installing htop -----"
@@ -120,7 +83,7 @@ echo ""
 echo "----- installing powershell -----"
 echo ""
 sudo apt install -y wget apt-transport-https software-properties-common
-wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/25.10/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt update
 sudo apt install -y powershell
@@ -130,11 +93,23 @@ echo ""
 echo "----- installing terraform -----"
 echo ""
 sudo apt install wget unzip -y
-TER_VER=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1')
-wget https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_linux_amd64.zip -O ~/terraform_${TER_VER}_linux_amd64.zip
-unzip ~/terraform_${TER_VER}_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
-rm ~/terraform_${TER_VER}_linux_amd64.zip
+ARCH=$(uname -m)
+TER_ARCH=""
+if [[ "$ARCH" == "x86_64" ]]; then
+    TER_ARCH="amd64"
+elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
+    TER_ARCH="arm64"
+fi
+
+if [[ -z "$TER_ARCH" ]]; then
+    echo "Warning: Unsupported architecture for Terraform ($ARCH). Skipping Terraform installation."
+else
+    TER_VER=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+    wget "https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_linux_${TER_ARCH}.zip" -O ~/terraform.zip
+    unzip -o ~/terraform.zip -d ~/
+    sudo mv ~/terraform /usr/local/bin/
+    rm ~/terraform.zip
+fi
 
 echo ""
 echo "----- installing sqlite -----"
